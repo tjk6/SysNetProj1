88e9dfa356afa41d02666d89e4cd5dff071b8e8d
tjk6 <tjk6@students.uwf.edu>
2018-02-18 00:59:29 -0600

    -Testing stage

---
+++ server.c
@@ -0,0 +1,6 @@
+//
+// Created by Tyler on 2/17/2018.
+//
+
+#include "server.h"
+
---
+++ server.h
@@ -0,0 +1,27 @@
+//
+// Created by Tyler on 2/17/2018.
+//
+
+#ifndef PROJECT1_SERVER_H
+#define PROJECT1_SERVER_H
+
+//#include <stddef.h>
+//
+//typedef struct addrinfo* AddrInfo;
+//
+//struct addrinfo {
+//    int ai_flags;
+//    int ai_family;
+//    int ai_socktype;
+//    int ai_protocol;
+//    size_t ai_addrlen;
+//    struct sockaddr *ai_addr;
+//    char *ai_canonname;
+//
+//    struct addrinfo *ai_next;
+//};
+
+
+
+
+#endif //PROJECT1_SERVER_H
